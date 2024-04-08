4c94afb31a1314335ed7efa8beba6c73fc3c63a6
Samp1997 <sampiazza2016@gmail.com>
2024-04-08 16:27:42 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -6,22 +6,21 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter three numbers");
+            Console.WriteLine("Please enter a number");
             int number = Convert.ToInt32(Console.ReadLine());
-            int number2 = Convert.ToInt32(Console.ReadLine());
-            int number3 = Convert.ToInt32(Console.ReadLine());
+            
             Console.ReadLine();
             //Taking each int and applying to the methods
             Console.WriteLine(MathOps.Math1(number));
             Console.ReadLine();
 
-            Console.WriteLine(MathOps.Math2(number2));
+            Console.WriteLine(MathOps.Math2(number));
             Console.ReadLine();
 
-            Console.WriteLine(MathOps.Math3(number3));
+            Console.WriteLine(MathOps.Math3(number));
             Console.ReadLine();
 
-            Console.WriteLine("All three numbers have been divided, multiplied and added by 5");
+            Console.WriteLine("The number selected has been divided, multiplied and added by 5");
         }
 
 
