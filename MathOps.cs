55e9e4987a99a5a84607fa2143b2bc64f3f2b09e
Samp1997 <sampiazza2016@gmail.com>
2024-04-08 15:11:06 -0700

    Add project files.

---
+++ MathOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public static class MathOps
+    {
+        public static int Math1(int value1)
+        {
+            return value1 / 5;
+        }
+
+        public static int Math2(int value)
+        {
+            return value * 5;
+        }
+
+        public static int Math3(int value)
+        {
+            return value + 5;
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(MathOps.Math1(number));
+            Console.ReadLine();
+        }
+
+
+    }
+}
