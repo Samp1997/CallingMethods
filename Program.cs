db6cb98644d26561916e49b68e598726dd5e03d9
Samp1997 <sampiazza2016@gmail.com>
2024-04-08 16:00:48 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -6,10 +6,19 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number");
+            Console.WriteLine("Please enter three numbers");
             int number = Convert.ToInt32(Console.ReadLine());
-
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            //Taking each int and applying to the methods
             Console.WriteLine(MathOps.Math1(number));
+            Console.ReadLine();
+
+            Console.WriteLine(MathOps.Math2(number2));
+            Console.ReadLine();
+
+            Console.WriteLine(MathOps.Math3(number3));
             Console.ReadLine();
         }
 
